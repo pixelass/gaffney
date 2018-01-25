46ecf1869d63d8bf691fefc017c53eafe9e4d378
Gregor Adams <greg@pixelass.com>
2018-01-25 01:47:08 +0100

    refactor(julian-day): simplify code

--- @gaffney/julian-day/src/index.js
+++ @gaffney/julian-day/src/index.js
@@ -1,18 +1,14 @@
-import {floor, parseInteger as int} from '@gaffney/utils'
+import {floor} from '@gaffney/utils'
 
 const julianDay = (year, month, day) => {
-  let y = year
-  if (y < 0) {
-    y++
-  }
-  let jy = int(y)
-  let jm = int(month) + 1
+  let jy = year
+  let jm = month + 1
   if (month <= 2) {
     jy--
     jm += 12
   }
-  let jul = floor(365.25 * jy) + floor(30.6001 * jm) + int(day) + 1720995
-  if (day + 31 * (month + 12 * y) >= 15 + 31 * (0 + 12 * 1582)) {
+  let jul = floor(365.25 * jy) + floor(30.6001 * jm) + day + 1720995
+  if (day + 31 * (month + 12 * year) >= 98099) {
     const ja = floor(0.01 * jy)
     jul = jul + 2 - ja + floor(0.25 * ja)
   }
